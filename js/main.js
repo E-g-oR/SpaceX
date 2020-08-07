6309fb0476367b6016c620f8dd7c8f23eaad8ad1
E-g-oR <68065928+E-g-oR@users.noreply.github.com>
2020-08-08 00:19:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,10 +8,15 @@ var rellax = new Rellax(".rocket");
 var MainMenuButton = document.querySelector('.menu-button'),
     SubMenuButton = document.querySelector('.sub-menu-button'),
     MainMenu = document.querySelector('.main-menu'),
-    SubMenu = document.querySelector('.sub-menu');
+    SubMenu = document.querySelector('.sub-menu'),
+    MenuButtonLine = document.querySelector('.menu-button_line');
 MainMenuButton.addEventListener('click', () => {
     MainMenu.classList.toggle('main-menu_active');
+    MainMenuButton.classList.toggle('main-menu_active');
+    MenuButtonLine.classList.toggle('main-menu_active');
+
 });
 SubMenuButton.addEventListener('click', () => {
     SubMenu.classList.toggle('sub-menu_active');
+    SubMenuButton.classList.toggle('sub-menu_active');
 });
