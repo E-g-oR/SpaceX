6ee009ecddd64f4c5d1dba5daf73443873a96b64
E-g-oR <68065928+E-g-oR@users.noreply.github.com>
2020-08-08 12:27:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,18 +5,29 @@ var instance = new vidbg(".video", {
   overlay: false // Boolean to display the overlay or not
 });
 var rellax = new Rellax(".rocket");
+
 var MainMenuButton = document.querySelector('.menu-button'),
     SubMenuButton = document.querySelector('.sub-menu-button'),
     MainMenu = document.querySelector('.main-menu'),
     SubMenu = document.querySelector('.sub-menu'),
     MenuButtonLine = document.querySelector('.menu-button_line');
+
 MainMenuButton.addEventListener('click', () => {
     MainMenu.classList.toggle('main-menu_active');
     MainMenuButton.classList.toggle('main-menu_active');
-    MenuButtonLine.classList.toggle('main-menu_active');
-
 });
 SubMenuButton.addEventListener('click', () => {
     SubMenu.classList.toggle('sub-menu_active');
     SubMenuButton.classList.toggle('sub-menu_active');
 });
+
+document.addEventListener('scroll', () => {
+    if (MainMenu.classList.contains('main-menu_active')) {
+        MainMenu.classList.remove('main-menu_active');
+        MainMenuButton.classList.remove('main-menu_active');
+    }
+    if (SubMenu.classList.contains('sub-menu_active')) {
+        SubMenu.classList.remove('sub-menu_active');
+        SubMenuButton.classList.remove('sub-menu_active');
+    }
+})
